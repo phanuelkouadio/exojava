3dc038772c4f27d26f5530dde9d72badd077b64f
phanuelkouadio <divinekouadio44@gmail.com>
2023-03-31 20:02:16 +0000

    test

---
+++ script.js
@@ -0,0 +1,115 @@
+EXERCICE4
+
+var somme= function(x=5,y=5,z=8){
+    return (x+y+z);
+}
+console.log(somme())
+
+
+EXERCICE2
+
+
+var misterynumber=4;
+var mynumber=2;
+ if(misterynumber==mynumber){
+     console.log("congratulations");
+}else if(misterynumber>mynumber){
+     console.log("+");
+}else{
+     console.log("-");
+ }
+
+
+
+ EXERCICE3
+
+ var a="1";
+var b="2";
+var c=a+b;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+
+console.log(parseInt(a));
+console.log(parseInt(b));
+
+
+
+
+
+
+EXERCICE7
+
+
+var salaire 
+var Sbrut;
+var sexe=Fe ||Ma;
+var PAC;
+
+const IR=0.18;
+const AE=0.07;
+const RP=0.05;
+const bonus=100;
+const allocution=150;
+
+
+ salaire=Sbrut-IR-AE-(RP+100) || salaire==Sbrut-IR-AE-(RP+150)
+ if(sexe=Fe){
+    salaire=Sbrut-2%
+    console.log("le salaire est:");
+ }
+ if(PAC=3){
+    salaire=Sbrut-3%
+    console.log("le salaire est:")
+ }
+ else{
+    (PAC=4)
+    salaire=Sbrut-2%
+    console.log("le salaire est:")
+ }
+
+
+
+
+
+EXERCICE1
+
+
+var value=1;
+if(typeof value=='number'){
+    console.log("la valeur est un nombre ")
+    
+}
+
+else {
+    console.log("la valeur est une chaine de caractre")
+}
+
+switch(value){
+   case 'value>0':
+       console.log('la valeur est superieur a 0');
+       break;
+     case 'value=0':
+       console.log('la valeur est egal a 0');
+       break;
+   
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
